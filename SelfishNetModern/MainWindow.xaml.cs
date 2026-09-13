ac6e5fc1affed731132ae8cb1b3f0d22127a9f9b
Sadkingo <hker0004@gmail.com>
2026-09-13 10:32:48 +0100

    feat(ui): remove default windows header, start maximized, and elevate button styles

--- SelfishNetModern/MainWindow.xaml.cs
+++ SelfishNetModern/MainWindow.xaml.cs
@@ -11,6 +11,37 @@ namespace SelfishNetModern
             InitializeComponent();
         }
 
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = (WindowState == WindowState.Maximized) ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_StateChanged(object? sender, System.EventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                MainContainer.Margin = new Thickness(8);
+                if (MaximizeIcon != null) MaximizeIcon.Text = "❐";
+                if (MaximizeBtn != null) MaximizeBtn.ToolTip = "Restore";
+            }
+            else
+            {
+                MainContainer.Margin = new Thickness(14);
+                if (MaximizeIcon != null) MaximizeIcon.Text = "🗖";
+                if (MaximizeBtn != null) MaximizeBtn.ToolTip = "Maximize";
+            }
+        }
+
         protected override void OnClosing(CancelEventArgs e)
         {
             if (DataContext is MainViewModel vm)
